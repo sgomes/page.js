c9003a68c70a2ab40667db31a62f077bbe44b38b
Matthew Phillips <matthew@matthewphillips.info>
2018-02-10 12:21:07 -0500

    Fix typo in the _getBase function

--- index.js
+++ index.js
@@ -113,13 +113,8 @@
   Page.prototype._getBase = function() {
     var base = this._base;
     if(!!base) return base;
-    var loc = hasWindow && this._window && ths._window.location;
+    var loc = hasWindow && this._window && this._window.location;
     return (hasWindow && this._hashbang && loc && loc.protocol === 'file:') ? loc.pathname : base;
-    var base = this._base;
-    if(!!base) return base;
-    var loc = hasWindow && this._window.location;
-    return (hasWindow && this._hashbang && loc.protocol === 'file:') ?
-      loc.pathname : base;
   };
 
   /**
